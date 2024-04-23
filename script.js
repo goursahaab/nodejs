f65d2e293392392b7f15085725995e4e8729329e
Nitin gour <gournitin39@gmail.com>
2024-04-23 10:17:02 +0530

    practice

---
+++ script.js
@@ -0,0 +1,23 @@
+const path = require("path");
+const os=require("os");
+const fs=requre("fs")
+
+// const desktoppath=path.join(os.homedir(),"desktop","dev"+Date.now());
+// 
+// try {
+//     fs.mkdirsync(path);
+//     fs.writeFileSync(path.join())
+// } catch (error) {
+//     .console.log("error.message");
+    
+// }
+
+
+// // fs.writeFileSync("index.html","index file");
+// // fs.writeFileSync("./style.css","style file");
+// // fs.mkdir("./")
+
+
+
+
+// server making...
